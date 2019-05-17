7f27a037d4521dacbc4aa0e3d2d8c2c22ce0ad62
jbshashank <shashijb@gmail.com>
2019-05-17 23:43:43 +0530

    Update Application.java

--- app/src/main/java/com/candidjava/Application.java
+++ app/src/main/java/com/candidjava/Application.java
@@ -10,12 +10,3 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-
-@SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-}

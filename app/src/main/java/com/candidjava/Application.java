bf593a77bc4d22655da5330911f15a88ca6a6dd1
jbshashank <shashijb@gmail.com>
2019-05-17 23:46:07 +0530

    Update Application.java

--- app/src/main/java/com/candidjava/Application.java
+++ app/src/main/java/com/candidjava/Application.java
@@ -3,10 +3,3 @@
 */
 
 package com.candidjava;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
